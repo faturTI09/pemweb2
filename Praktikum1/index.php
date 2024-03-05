a03470d3a9b5a6f81f0d38b8b126a1c5634fbcc1
faturTI09 <fatu5044@gmail.com>
2024-03-04 20:31:29 -0800

    saya mencoba push tugas pertama saya

---
+++ Praktikum1/index.php
@@ -0,0 +1,10 @@
+<?php
+// membuat variabel user 
+$name = "Faturrahman";
+$age = 18;
+$weight = 60;
+
+echo $name;
+echo "<br> saya berusia". $age;
+
+?>
---
+++ Praktikum2/submit_get.php
@@ -0,0 +1,9 @@
+<?php
+
+$namaLengkap = $_GET['nama-lengkap'];
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+echo "<h1>Selamat {$namaLengkap}, Kamu sudah terdaftar</h1>";
+echo "<h3>Username : {$username}</h3>";
+echo "<h3>Password : {$password}</h3>";
