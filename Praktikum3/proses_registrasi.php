617703f25fe7a316fdc6792ea139ed170a9655dd
faturTI09 <fatu5044@gmail.com>
2024-03-07 19:58:00 -0800

    pesan di github (bebas)

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,13 @@
+<?php
+//Array Numerik - Index & Value
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi", "Bima NTB"];
+
+//Array Assosiative - Key & Value
+$program_studi = [
+    "SI" => "Siste Imformasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital"
+];
+
+
+?>
