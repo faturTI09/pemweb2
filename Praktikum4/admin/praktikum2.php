e0d42004027884018e72f7a7ed4bdd164a1b0ba7
faturTI09 <162272114+faturTI09@users.noreply.github.com>
2024-03-14 21:25:11 -0700

    Add files via upload

---
+++ Praktikum4/admin/praktikum2.php
@@ -0,0 +1,25 @@
+<?php 
+require_once 'navbar.html';
+require_once 'sidebar.html';
+?>
+<h1>Praktikum 2</h1>
+<h3>Form Registrasi Pengguna</h3>
+    <form action="Submit_get.php" method="GET">
+        <div>
+            <label for="nama_lengkap">Nama Lengkap</label>
+            <input type="text" name="nama_lengkap" id="nama_lengkap" required>
+        </div><br>
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" required>
+        </div><br>
+        <div>
+            <label for="password">Password</label>
+            <input type="paswword" name="password" id="password" required>
+        </div><br>
+        <div>
+        <button type="submit">Register</button>
+    </div>
+</form>
+
+<?php require_once 'footer.html'; ?>
